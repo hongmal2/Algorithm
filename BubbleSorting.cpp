c2d3a7eccd527b0066cf848b75fb0413cbd693d5
hongmal2 <84785444+hongmal2@users.noreply.github.com>
2022-05-04 02:10:24 +0900

    220504 알고리즘 학습

---
+++ BubbleSorting.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int number = 10;
+int data[] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+
+void show() {
+	int i;
+	for(i = 0; i < number; i++){
+		printf("%d", data[i]);
+	}
+}
+
+void quickSort(int* data, int start, int end) {
+	if(start >= end) { // 원소가 1개인 경우 그대로 둔다. 
+		return;
+	}
+	
+	int pivot = start; // pivot은 첫 번째 원소 
+	int i = start + 1, j = end, temp;
+	
+	while(i <= j) { // 엇갈릴 때까지 반복
+		while(i <= end && data[1] <= data[pivot]) { // pivot 값보다 큰 값을 만날 때까지 
+			i++;
+		}
+		while(j > start && data[j] >= data[pivot]) { // pivot 값보다 작은 값을 만날 때까지 
+			j--;
+		}
+		if(i > j) { // 현재 엇갈린 상태면 pivot 값과 교체 
+			temp = data[j];
+			data[j] = data[pivot];
+			data[pivot] = temp;
+		}
+		else { // 엇갈리지 않았으면 i와 j를 교체 
+			temp = data[i];
+			data[i] = data[j];
+			data[j] = temp;
+		}
+	}
+	
+	quickSort(data, start, j - 1);
+	quickSort(data, j + 1, end);
+}
+
+int main(void) {
+	quickSort(data, 0, number - 1);
+	show();
+	return 0;
+}
+	
